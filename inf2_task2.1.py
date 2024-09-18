2ac7aa3e6dfb8e21f198a7371b1b175257ce2818
Gesha <baramasya@gmail.com>
2024-09-18 16:57:49 +0300

    fix

---
+++ inf2_task2.1.py
@@ -0,0 +1,5 @@
+a=str(input())
+if '3' in a:
+    print('yep')
+else:
+    print('nope')
---
+++ inf2_task2.2.py
@@ -0,0 +1,7 @@
+a=str(input())
+if int(a[0])>int(a[1]):
+    print('1st')
+elif int(a[0])<int(a[1]):
+    print('2nd')
+else:
+    print('=')
