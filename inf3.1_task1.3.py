293bca4140cc3f2a8488f366d5d470572ea16464
Gesha <baramasya@gmail.com>
2024-10-18 09:56:13 +0300

    /

--- inf3.1_task1.3.py
+++ inf3.1_task1.3.py
@@ -3,10 +3,9 @@ from math import *
 def f(x):
     return (cos(exp(1)*x))**3 + sin(abs(x))
 
-a = int(input())
+a = int(input()) 
 b = int(input())
 h = int(input())
 x=a
 for x in range(a,b+1,h*x):
     print(f(x))
-#неправильно считает??
